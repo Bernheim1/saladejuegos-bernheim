bf0e207c24debd729d292214d1b7b75a1e8c54f3
Bernheim1 <agustinbernheim8@gmail.com>
2021-09-05 19:32:49 -0300

    Sprint 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { RegistroComponent } from './pagina/registro/registro.component';
 
 const routes: Routes = [
 
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'home', component: HomeComponent},
     {path: 'QuienSoy', component: QuiensoyComponent},
