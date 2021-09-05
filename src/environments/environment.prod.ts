115c578314083e2da3a594818ef5006aafcc1b1c
Bernheim1 <agustinbernheim8@gmail.com>
2021-09-05 19:25:47 -0300

    Sprint 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCrDVe8QiR4lw_V9yWOujy3eYUuG1JEtWI",
+    authDomain: "sala-de-juegos-b8607.firebaseapp.com",
+    projectId: "sala-de-juegos-b8607",
+    storageBucket: "sala-de-juegos-b8607.appspot.com",
+    messagingSenderId: "335311365824",
+    appId: "1:335311365824:web:721038b0d6cb56dec46f25"
+  }
 };
